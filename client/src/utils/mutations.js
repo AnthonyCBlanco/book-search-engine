2a5f2b8936fbdb778373b7aa91804ae380478d32
Anthony Blanco <anthony200527@outlook.com>
2024-03-27 14:18:30 -0700

    Fixed mutations names

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,6 +1,6 @@
 import { gql } from '@apollo/client'
 
-export const loginUser = gql`
+export const LOGIN_USER = gql`
     mutation login($email: String!, $password: String!) {
         login(email: $email, password: $password) {
         token
@@ -12,7 +12,7 @@ export const loginUser = gql`
     }
 `
 
-export const addUser = gql`
+export const ADD_USER = gql`
     mutation addUser($username: String!, $email: String!, $password: String!) {
         addUser(username: $username, email: $email, password: $password) {
         token
@@ -24,7 +24,7 @@ export const addUser = gql`
     }
 `
 
-export const saveBook = gql`
+export const SAVE_BOOK = gql`
     mutation saveBook($bookData: BookInput!) {
         saveBook(bookData: $bookData) {
         _id
@@ -42,7 +42,7 @@ export const saveBook = gql`
     }
 `
 
-export const removeBook = gql`
+export const REMOVE_BOOK = gql`
     mutation removeBook($bookId: ID!) {
     removeBook(bookId: $bookId) {
         _id
