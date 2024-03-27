5db383dfaf46cc12556778119cb9f78eb117b235
Anthony Blanco <anthony200527@outlook.com>
2024-03-27 14:10:02 -0700

    Set up Apollo Client in App.jsx

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -13,49 +13,49 @@ export const loginUser = gql`
 `
 
 export const addUser = gql`
-  mutation addUser($username: String!, $email: String!, $password: String!) {
-    addUser(username: $username, email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
-      }
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+        token
+        user {
+            _id
+            username
+        }
+        }
     }
-  }
-`;
+`
 
 export const saveBook = gql`
-  mutation saveBook($bookData: BookInput!) {
-    saveBook(bookData: $bookData) {
-      _id
-      username
-      email
-      savedBooks {
-        bookId
-        authors
-        image
-        description
-        title
-        link
-      }
+    mutation saveBook($bookData: BookInput!) {
+        saveBook(bookData: $bookData) {
+        _id
+        username
+        email
+        savedBooks {
+            bookId
+            authors
+            image
+            description
+            title
+            link
+        }
+        }
     }
-  }
-`;
+`
 
 export const removeBook = gql`
-  mutation removeBook($bookId: ID!) {
+    mutation removeBook($bookId: ID!) {
     removeBook(bookId: $bookId) {
-      _id
-      username
-      email
-      savedBooks {
+        _id
+        username
+        email
+        savedBooks {
         bookId
         authors
         image
         description
         title
         link
-      }
+        }
     }
-  }
-`;
+    }
+    `
